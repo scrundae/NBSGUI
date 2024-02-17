71a6a18ed6a58a01f1494c76157e45f831f72619
Harry Collins <sensorydog@gmail.com>
2024-02-17 15:39:04 +1000

    stuff

---
+++ NBSGUI/Resource.cs
@@ -0,0 +1,17 @@
+﻿using NBSGUI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBSGUI
+{
+    namespace Globals
+    {
+        public static class Resource
+        {
+            public static Frame? DefaultFrame = null;
+        }
+    }
+}
